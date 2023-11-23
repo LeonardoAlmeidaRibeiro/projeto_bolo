e4320a5eeafad0ebf75d203af26c856a4653b659
Leonardo Ribeiro <leonardoribeiro@hfa.mil.br>
2023-11-23 15:26:33 -0300

    teste de rotas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\Utils\SpaceController;
+use App\Http\Controllers\Utils\Space2Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('photos', PhotoController::class);
+Route::resource('space', SpaceController::class);
+Route::resource('space', Space2Controller::class);
